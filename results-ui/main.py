8990c2b19344b86bcb4143ac946de57becf6d09e
Gerard Casas Saez <casassg@gmail.com>
2019-01-15 10:21:11 -0700

    add separate view for each event

--- results-ui/main.py
+++ results-ui/main.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import re
 
 import google
 from flask import Flask, render_template, json
@@ -21,19 +20,7 @@ logging.basicConfig(
 PRODUCTION = os.environ.get('PRODUCTION', False)
 
 
-def slugify(value):
-    """
-    Normalizes string, converts to lowercase, removes non-alpha characters,
-    and converts spaces to hyphens.
-    """
-    import unicodedata
-    value = unicodedata.normalize('NFKD', unicode(value)).encode('ascii', 'ignore')
-    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
-    value = unicode(re.sub('[-\s]+', '-', value))
-    return value
-
-
-def _get_uploaded_users(bucket_name, folder):
+def _get_uploaded_users(bucket_name, folder, event=None):
     logging.info('Getting uploaded users %s...' % bucket_name)
     cli = storage.Client()
     try:
@@ -43,10 +30,30 @@ def _get_uploaded_users(bucket_name, folder):
             splitted = blob.name.split('/')
             if len(splitted) < 4:
                 continue
+            if event and event != splitted[1]:
+                continue
             url = 'https://storage.googleapis.com/%s/%s/%s/%s/%s' % (
-            bucket_name, folder, splitted[1], splitted[2], splitted[3])
-            d.append({'event': splitted[1], 'status': splitted[2], 'user': splitted[3], 'url':url})
+                bucket_name, folder, splitted[1], splitted[2], splitted[3]
+            )
+            d.append({'event': splitted[1], 'status': splitted[2], 'user': splitted[3], 'url': url})
         return d
+    except google.cloud.exceptions.NotFound:
+        logging.error('Bucket %s does not exist!' % BUCKET_NAME)
+    return {}
+
+
+def _get_uploaded_events(bucket_name):
+    logging.info('Getting uploaded events %s...' % bucket_name)
+    cli = storage.Client()
+    try:
+        bucket = cli.get_bucket(bucket_name)
+        d = set()
+        for blob in bucket.list_blobs():
+            splitted = blob.name.split('/')
+            if len(splitted) < 4:
+                continue
+            d.add(splitted[1])
+        return {'events': list(d)}
     except google.cloud.exceptions.NotFound:
         logging.error('Bucket %s does not exist!' % BUCKET_NAME)
     return {}
@@ -54,22 +61,42 @@ def _get_uploaded_users(bucket_name, folder):
 
 @app.route('/')
 def home():
-    return render_template('users.html')
+    return render_template('home.html', event='')
 
 
-@app.route('/geo')
-def geohome():
-    return render_template('geo_users.html')
+@app.route('/event/<event>')
+def home_event(event):
+    return render_template('users.html', event=event)
 
 
-@app.route('/data')
+@app.route('/geo/event/<event>')
+def geohome_event(event):
+    return render_template('geo_users.html', event=event)
+
+
+@app.route('/data/')
 def get_data():
     return json.dumps(_get_uploaded_users(BUCKET_NAME, 'results'))
 
 
-@app.route('/geodata')
+@app.route('/data/<event>')
+def get_data_event(event=''):
+    return json.dumps(_get_uploaded_users(BUCKET_NAME, 'results', event=event))
+
+
+@app.route('/geodata/')
 def get_geodata():
     return json.dumps(_get_uploaded_users(GEOBUCKET_NAME, 'georesults'))
+
+
+@app.route('/geodata/<event>')
+def get_geodata_event(event):
+    return json.dumps(_get_uploaded_users(GEOBUCKET_NAME, 'georesults', event=event))
+
+
+@app.route('/events/')
+def overall_events():
+    return json.dumps(_get_uploaded_events(BUCKET_NAME))
 
 
 if __name__ == "__main__":
